ae35d2db1fe4723377108dd418b52ba59cb4cab9
Nat Pryce <sw@natpryce.com>
2018-06-06 07:12:02 +0100

    Simplify nulls test

--- exercise/learnyouakotlin/part1a/NullsTests.kt
+++ exercise/learnyouakotlin/part1a/NullsTests.kt
@@ -9,6 +9,9 @@ import kotlin.test.assertNull
 private val learnYouAKotlin = Session("Learn you a kotlin", "for all the good it will do you", Slots(1, 1))
 private val refactoringToStreams = Session("Refactoring to Streams", null, Slots(2, 2))
 
+fun List<Session>.sessionWithTitle(s: String): Session? =
+    find { it.title.equals(s, ignoreCase = true) }
+
 fun subtitleOf(session: Session?): String? {
     if (session == null) return null
     return session.subtitle
@@ -19,24 +22,18 @@ fun subtitleOrPrompt(session: Session?): String {
     return session.subtitle
 }
 
-object Sessions {
-    fun sessionWithTitle(s: String): Session? {
-        return when (s.toLowerCase()) {
-            learnYouAKotlin.title.toLowerCase() -> learnYouAKotlin
-            refactoringToStreams.title.toLowerCase() -> refactoringToStreams
-            else -> null
-        }
-    }
-}
-
 class NullsTests {
+    val sessions = listOf(learnYouAKotlin, refactoringToStreams)
+    
     @Test
     fun nulls() {
-        val session: Session? = Sessions.sessionWithTitle("learn you a kotlin")
+        val session: Session? = sessions.sessionWithTitle("learn you a kotlin")
+        
         val notNullSession = session as Session
         assertEquals("for all the good it will do you", notNullSession.subtitle)
         assertEquals("for all the good it will do you", session.subtitle)
     }
+    
     
     @Test
     fun null_safe_access() {
